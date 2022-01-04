1cd3ca1ac7beb624ddc8629a764720baf2176a13
pundirAbhishek <abhishekpundir526@gmail.com>
2022-01-05 00:42:06 +0530

    Added CryptoHomeViewModel

--- app/src/main/java/com/example/crypto/home/CryptoHomeViewModel.kt
+++ app/src/main/java/com/example/crypto/home/CryptoHomeViewModel.kt
@@ -2,8 +2,8 @@ package com.example.crypto.home
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import com.example.crypto.network.Retrofit
 
 class CryptoHomeViewModel(context: Context) : ViewModel() {
-
+    private val cryptoRepository = Retrofit.createRepository(context = context)
 }
