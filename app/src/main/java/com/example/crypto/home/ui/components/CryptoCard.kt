62a8fac48b277d0f703d79f25abeee4d97bce8ed
pundirAbhishek <abhishekpundir526@gmail.com>
2022-01-07 20:41:46 +0530

    Crypto Card UI Refactor

--- app/src/main/java/com/example/crypto/home/ui/components/CryptoCard.kt
+++ app/src/main/java/com/example/crypto/home/ui/components/CryptoCard.kt
@@ -5,10 +5,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,10 +34,22 @@ fun CryptoCard(modifier: Modifier = Modifier) {
     Card(
         modifier = modifier
             .clip(RoundedCornerShape(16.dp))
-            .width(240.dp)
+            .width(220.dp)
+
+
     ) {
         Column(
-            modifier = Modifier.padding(horizontal = 20.dp, vertical = 16.dp)
+            modifier = Modifier
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            red,
+                            green200
+                        )
+                    )
+                )
+                .padding(horizontal = 20.dp, vertical = 16.dp)
+
         ) {
             Row(
                 horizontalArrangement = Arrangement.SpaceBetween,
@@ -46,23 +62,26 @@ fun CryptoCard(modifier: Modifier = Modifier) {
 
             val data = chartData.map {
                 it.toFloat()
-            }
+            }.subList(0, (chartData.size * 0.2).toInt())
 
             LineChart(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(150.dp),
                 yAxisValues = data,
-                lineColors = gradientGreenColors
+                lineColors = listOf(Color.White, Color.White)
             )
 
             Row(
                 horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.fillMaxWidth(1f)
+
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.eth),
-                    contentDescription = "Crypto Logo"
+                    contentDescription = "Crypto Logo",
+                    modifier = Modifier.size(60.dp)
                 )
 
                 Column {
