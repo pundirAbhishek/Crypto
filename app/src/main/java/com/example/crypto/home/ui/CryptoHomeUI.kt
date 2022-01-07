c8a6d62771c4cc538deb650718e5aa7d2bbeaeae
pundirAbhishek <abhishekpundir526@gmail.com>
2022-01-08 00:51:05 +0530

    Added Snapping Effect in Crypto Card using Snapper Library.

--- app/src/main/java/com/example/crypto/home/ui/CryptoHomeUI.kt
+++ app/src/main/java/com/example/crypto/home/ui/CryptoHomeUI.kt
@@ -1,20 +1,29 @@
 package com.example.crypto.home.ui
 
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.crypto.home.ui.components.CryptoCard
+import com.example.crypto.snapper.ExperimentalSnapperApi
+import com.example.crypto.snapper.rememberSnapperFlingBehavior
 
+@ExperimentalSnapperApi
 @Composable
 fun CryptoHomeUI() {
-    LazyRow{
-        items(5){ lazyItemScope ->
+    val lazyListState = rememberLazyListState()
+    LazyRow(
+        state = lazyListState,
+        flingBehavior = rememberSnapperFlingBehavior(lazyListState = lazyListState)
+    ) {
+        items(5) {
             CryptoCard()
         }
     }
 }
 
 
+@ExperimentalSnapperApi
 @Preview
 @Composable
 fun CryptoHomeUIPreview() {
